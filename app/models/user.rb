07518d601d8394076f85f9aefb0f99561aa453b7
Hiroshi Goto <hiroshi.goto@runthink.co.jp>
2019-08-24 20:30:35 +0900

    Implemented Custom full name method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 end
